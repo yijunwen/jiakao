3c2039272f0cfbc8c2d2d7479f734a8ca9b9602a
[wen.yijun] <1049025904@qq.com>
2020-12-09 00:41:15 +0800

    静态资源导入

--- src/main/java/com/wyj/jk/controller/DictTypeController.java
+++ src/main/java/com/wyj/jk/controller/DictTypeController.java
@@ -4,11 +4,10 @@ import com.wyj.jk.pojo.po.DictType;
 import com.wyj.jk.service.DictTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
 
 /**
  * @author wen.yijun
@@ -24,9 +23,9 @@ public class DictTypeController {
     DictTypeService dictTypeService;
 
     @GetMapping("/list")
-    @ResponseBody
-    public List<DictType> list(){
-        return dictTypeService.list();
+    public String list(Model model){
+        model.addAttribute("data",dictTypeService.list());
+        return "pages/dictType";
     }
 
 }
